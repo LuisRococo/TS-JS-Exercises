3016bb5ed305d3eea152d752c711755a2a6a2dc2
Luis Roberto <luisrobertopenav@gmail.com>
2024-06-05 14:33:59 -0700

    learn about generics

--- learning/mixins.ts
+++ learning/mixins.ts
@@ -1,1 +1,39 @@
-// Pending
+/*
+Basically allows you to mix two or more classes
+*/
+
+type Constructor<T = {}> = new (...args: any[]) => T;
+
+const Sortable = <TBase extends Constructor>(Base: TBase) => {
+  return class extends Base {
+    public sort() {
+      console.log("Sorting....");
+    }
+  };
+};
+
+const Activable = <TBase extends Constructor>(Base: TBase) => {
+  return class extends Base {
+    public active: boolean;
+    constructor(...args: any[]) {
+      super(...args);
+      this.active = true;
+    }
+
+    public activate() {
+      this.active = true;
+    }
+
+    public deactivate() {
+      this.active = false;
+    }
+  };
+};
+
+class UtilityClass {}
+
+class Combined extends Activable(Sortable(UtilityClass)) {}
+
+const instance = new Combined();
+instance.activate();
+instance.sort();
